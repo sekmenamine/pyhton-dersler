d95299f753916e9139a06e10e433811901d4a661
Amine Sekmenoğlu <sekmenamine@gmail.com>
2025-05-05 17:14:32 +0300

    added discord bot

--- bot.py
+++ bot.py
@@ -1,3 +1,25 @@
+import discord
 
+intents = discord.Intents.default()
+intents.message_content=True
+client = discord.Client(intents=intents)
 
+@client.event
+async def on_ready():
+    print(f'{client.user} olarak giriş yaptı.')
 
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith("merhaba"):
+        await message.channel.send("Selam!")
+    elif message.content.startswith("bye"):
+        await message.channel.send("\U0001f642")
+    elif message.content.startswith("selamun aleyküm tostum"):
+        await message.channel.send("Aleyküm Selam Tostummmmmm!!!🤲🤝")
+    else:
+        await message.channel.send(message.content)
+
+client.run("--token--")            
